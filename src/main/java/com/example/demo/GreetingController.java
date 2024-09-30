7759cece15d8ab4085cbd16b16646a1391ad587d
JivenGG <jokermanjrj@gmail.com>
2024-09-30 12:31:14 +0200

    Funcionamiento basico completado

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController
+public class GreetingController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", defaultValue = "Guest") String name, Locale locale) {
+        String greetingMessage = messageSource.getMessage("greeting.message", new Object[]{name}, locale);
+        return greetingMessage;
+    }
+}
